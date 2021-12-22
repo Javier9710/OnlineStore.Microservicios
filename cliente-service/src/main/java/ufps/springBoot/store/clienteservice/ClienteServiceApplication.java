c948172f7e6cf750988a13c2e4afbcbe05b711ba
Javier Moncada <javierandresmd@ufps.edu.co>
2021-12-21 23:17:45 -0500

    cliente service actualizado como client de eureka

--- cliente-service/src/main/java/ufps/springBoot/store/clienteservice/ClienteServiceApplication.java
+++ cliente-service/src/main/java/ufps/springBoot/store/clienteservice/ClienteServiceApplication.java
@@ -2,8 +2,11 @@ package ufps.springBoot.store.clienteservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ClienteServiceApplication {
 
 	public static void main(String[] args) {
